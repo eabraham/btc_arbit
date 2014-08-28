b9f2d83671b8f731b3402a53838f540e51698f44
Eric Abrahamsen <eric@ploonky.com>
2014-08-28 00:44:45 -0400

    Fix for improper memoize price.

--- lib/rbtc_arbitrage/clients/rock_client.rb
+++ lib/rbtc_arbitrage/clients/rock_client.rb
@@ -14,45 +14,31 @@ module RbtcArbitrage
         :rock
       end
 
-      def hmac_512(msg)
-	hmac = HMAC::SHA1.new(ENV['LAKEBTC_KEY'])
-	signature = hmac.update(msg)
-	Base64.encode64("#{ENV['LAKEBTC_ACCESS_KEY']}:#{hmac.update(msg).to_s}").chomp.gsub( /\n/, '' )
-      end
-
       # Returns an array of Floats.
       # The first element is the balance in BTC;
       # The second is in USD.
       def balance
-        #return [100,100]
-	tonce=DateTime.now.strftime('%Q')
-	id = 1
-	values   = ("tonce=#{tonce}&"+
-			        "accesskey=#{ENV['LAKEBTC_ACCESS_KEY']}&"+
-                                "requestmethod=post&"+
-                                "id=#{id}&"+
-                                "method=getAccountInfo&"+
-                                "params=")
-        sign = hmac_512(values)
+        params = {'username' => ENV['ROCK_USERNAME'],
+		  'password' => ENV['ROCK_PASSWORD'],
+                  'api_key'  => ENV['ROCK_KEY']}
 
-	headers  = {'Json-Rpc-Tonce' => tonce,
-		    'Authorization'=> "Basic #{sign}"}
-        params = {'username':ENV['ROCK_USERNAME'],
-		  'password':ENV['ROCK_PASSWORD'],
-                  'api_key':ENV['ROCK_KEY'],
-	          'type_of_currency':'BTCUSD'}
+        btc_balance = 0.0
+	usd_balance = 0.0
 
-	response = RestClient.post("https://www.therocktrading.com/api/get_balance", params.to_json, headers)
+	response = RestClient.post("https://www.therocktrading.com/api/get_balance", params, {})
 	r_json = JSON.parse(response)
-	binding.pry
-	if (r_json.keys.include?("balance"))
-      	  btc_balance = r_json["balance"]["BTC"].to_f
-          usd_balance = r_json["balance"]["USD"].to_f
-        else
+	if (r_json.keys.include?("result"))
+	  r_json["result"].each do |currency|
+            if currency["currency"]=="BTC"
+      	      btc_balance = currency["balance"].to_f
+	    elsif currency["currency"]=="USD"
+	      usd_balance = currency["balance"].to_f
+            end
+	  end
+    	else
 	  btc_balance = 0.0
 	  usd_balance = 0.0
 	end
-
         return [btc_balance, usd_balance]
       end
 
@@ -61,7 +47,7 @@ module RbtcArbitrage
 
       # Configures the client's API keys.
       def validate_env
-        validate_keys :lakebtc_access_key, :lakebtc_key
+        validate_keys :rock_key, :rock_username, :rock_password
       end
 
       # `action` is :buy or :sell
@@ -70,22 +56,19 @@ module RbtcArbitrage
 	p = @price[:bid] if @price && @price[:bid] #memoize
 	p = @price[:ask] if @price && @price[:ask] #memoize
 	p = price(action) unless p
-        bid_ask=action==:buy ? "buyOrder":"sellOrder"
+        bid_ask=action==:buy ? "S":"B" #reverse because its BTCUSD pair
 	volume = @options[:volume]
 
-        tonce=DateTime.now.strftime('%Q')
-        id = 1
-	values   = ("tonce=#{tonce}&"+
-                    "accesskey=#{ENV['LAKEBTC_ACCESS_KEY']}&"+
-		    "requestmethod=post&"+
-		    "id=#{id}&"+
-		    "method=#{bid_ask}&"+
-		    "params=#{[p,volume,'USD'].split(',')}")
-	sign = hmac_512(values)
+        params = {'username' => ENV['ROCK_USERNAME'],
+	          'password' => ENV['ROCK_PASSWORD'],
+	          'api_key'  => ENV['ROCK_KEY'],
+	          'fund_name'=> 'BTCUSD',
+		  'order_type'=>bid_ask,
+		  'amount'   => volume,
+		  'price'    => p
+	         }
 
-	headers  = {'Json-Rpc-Tonce' => tonce,
-		    'Authorization'=> "Basic #{sign}"}
-	response = RestClient.post("https://www.LakeBTC.com/api_v1", {params:[p,volume,'USD'],method:"#{bid_ask}",id:id}.to_json, headers)
+	response = RestClient.post("https://www.therocktrading.com/api/place_order", params, {})
 
       end
 
@@ -93,29 +76,25 @@ module RbtcArbitrage
       # Returns a Numeric type.
       def price action
 	bid_ask=action==:buy ? :ask : :bid
-	return @price[:bid] if @price && @price[:bid] #memoize
-        return @price[:ask] if @price && @price[:ask] #memoize
-        response = RestClient.get "https://www.LakeBTC.com/api_v1/ticker"
+	return @price[:bid] if !@price.nil? && !@price[:bid].nil? && bid_ask==:bid #memoize
+        return @price[:ask] if !@price.nil? && !@price[:ask].nil? && bid_ask==:ask #memoize
+        response = RestClient.get "https://www.therocktrading.com/api/tickers"
 	r_json = JSON.parse(response)
-
-        if (r_json.keys.include?("USD"))
-	  if bid_ask==:ask
-            rate = r_json["USD"]["ask"].to_f
-          elsif bid_ask==:bid
-            rate = r_json["USD"]["bid"].to_f
-          end
+        if (r_json.keys.include?("result") && r_json["result"].keys.include?("tickers"))
+          @price[:ask]=r_json["result"]["tickers"]["BTCUSD"]["ask"].to_f
+	  @price[:bid]=r_json["result"]["tickers"]["BTCUSD"]["bid"].to_f
         else
-          rate = 0
+          @price = {:ask=>0, :bid=>0}
         end
 
-	@price[bid_ask] = rate
+	@price[bid_ask]
       end
 
       # Transfers BTC to the address of a different
       # exchange.
       def transfer client
         if @options[:verbose]
-          error = "Kraken does not have a 'transfer' API.\n"
+          error = "Rock Trading does not have a 'transfer' API.\n"
           error << "You must transfer bitcoin manually."
 	  @options[:logger].error error
 	end
@@ -125,30 +104,6 @@ module RbtcArbitrage
       # BTC address, implement this, otherwise
       # remove this method and set the ENV
       # variable [this-exchange-name-in-caps]_ADDRESS
-      def address
-
-        tonce=DateTime.now.strftime('%Q')
-        id = 1
-        values   = ("tonce=#{tonce}&"+
-	            "accesskey=#{ENV['LAKEBTC_ACCESS_KEY']}&"+
-		    "requestmethod=post&"+
-		    "id=#{id}&"+
-		    "method=getAccountInfo&"+
-		    "params=")
-	sign = hmac_512(values)
-							
-        headers  = {'Json-Rpc-Tonce' => tonce,
-		    'Authorization'=> "Basic #{sign}"}
-	response = RestClient.post("https://www.LakeBTC.com/api_v1", {params:[],method:"getAccountInfo",id:id}.to_json, headers)
-	r_json = JSON.parse(response)
-	if (r_json.keys.include?("profile"))
-          btc_address = r_json["profile"]["btc_deposit_addres"]
-	else
-	  btc_address = ""
-	end
-
-	return btc_address
-      end
     end
   end
 end
